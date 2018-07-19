2dac081741a9568f80c5487420a00972df7eb9fb
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-19 11:16:12 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 50}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: true
+  validates :category, inclusion: {in: %w(Category)}
 end
