d16ffc428ee6c277bbc3307890a2d3457a56ca44
Eric DeSimone <eric.desimone@cortland.edu>
2018-07-19 11:17:46 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ActiveRecord::Base
+  @@categories = ["fiction"]
+
   validates :title, presence: true
   validates :content, length: {minimum: 50}
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: {in: @@categories}
 
-  @@categories = ["fiction"]
+  
 end
